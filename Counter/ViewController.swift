a0469191ef89917abcef1a14665f7929a32b8910
Чупрыненко Андрей <wotmaster187@icloud.com>
2023-06-12 19:56:05 +0300

    Был добавлен лейбл с текстом 0, добавлена кнопка, которая увеличивает значение лейбла на 1

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count: Int = 0
+    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var countButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonCount(_ sender: Any) {
+        count += 1
+        counter.text = String(count)
+        print(count)
+    }
 }
 
